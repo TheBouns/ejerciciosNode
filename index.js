0e9b534415f2007870d9c0557045d2b1f654e710
TheBouns <ilbonsi3@hotmail.com>
2022-01-13 16:33:49 +0100

    abrir para disfrutar update

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Mira mama he hecho un Script!!!")
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+
+http.createServer((req, res) => {
+    const q = url.parse(req.url, true);
+    const filename = `./html/${q.pathname}`;
+    fs.readFile(filename, (err, data) => {
+      try {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      } catch (error) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        console.error(error);
+        return res.end("404 Not Found");
+      }
+    });
+  }).listen(3000);
